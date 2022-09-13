fa539daf89ad5c76a1b663633154833a2472b62e
Satya Prakash Dash <sprakashdash@gmail.com>
2022-09-14 00:08:56 +0530

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,37 @@
+python3 legged_gym/scripts/train.py --task=a1 --headless --seed=100
+wait 
+python3legged_gym/scripts/train.py --task=a1 --headless --seed=102 
+wait
+python3 legged_gym/scripts/train.py --task=a1 --headless --seed=105 
+wait
+python3 legged_gym/scripts/train.py --task=a1 --headless --seed=108 
+wait
+python3 legged_gym/scripts/train.py --task=a1 --headless --seed=110 
+wait
+python3 legged_gym/scripts/train.py --task=a1 --headless --seed=10020 
+wait
+python3 legged_gym/scripts/train.py --task=a1 --headless --seed=104 
+wait
+python3 legged_gym/scripts/train.py --task=a1 --headless --seed=112 
+wait
+python3 legged_gym/scripts/train.py --task=a1 --headless --seed=1023 
+wait
+python3 legged_gym/scripts/train.py --task=a1 --headless --seed=100 
+wait
+python3 legged_gym/scripts/train.py --task=a1 --headless --seed=100 
+wait
+python3 legged_gym/scripts/train.py --task=a1 --headless --seed=100 
+wait
+python3 legged_gym/scripts/train.py --task=a1 --headless --seed=100 
+wait
+python3 legged_gym/scripts/train.py --task=a1 --headless --seed=100 
+wait
+python3 legged_gym/scripts/train.py --task=a1 --headless --seed=1006 
+wait
+python3 legged_gym/scripts/train.py --task=a1 --headless --seed=1004 
+wait
+python3 legged_gym/scripts/train.py --task=a1 --headless --seed=1006 
+wait
+python3 legged_gym/scripts/train.py --task=a1 --headless --seed=1008
+wait 
+python3 legged_gym/scripts/train.py --task=a1 --headless --seed=100
